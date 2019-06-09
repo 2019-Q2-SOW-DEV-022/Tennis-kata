ebc60df0c29dc72c7734335fb84d006024c9e2e1
2019-Q2-SOW-DEV-022 <email@email.com>
2019-06-09 19:52:53 +0530

    Implement new game setup with two player names

--- src/main/java/com/personal/kata/TennisGame.java
+++ src/main/java/com/personal/kata/TennisGame.java
@@ -3,6 +3,13 @@ package com.personal.kata;
 public class TennisGame {
     private int player1Score;
     private int player2Score;
+    private final String player1Name;
+    private final String player2Name;
+
+    public TennisGame() {
+        this.player1Name = "Player 1";
+        this.player2Name = "Player 2";
+    }
 
     public int getPlayer1Score() {
         return player1Score;
@@ -10,5 +17,13 @@ public class TennisGame {
 
     public int getPlayer2Score() {
         return player2Score;
+    }
+
+    public String getPlayer1Name() {
+        return player1Name;
+    }
+
+    public String getPlayer2Name() {
+        return player2Name;
     }
 }
