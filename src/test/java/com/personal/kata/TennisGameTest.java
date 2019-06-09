baafb47229a15275761bbe1af5086f8d3f00a71c
2019-Q2-SOW-DEV-022 <email@email.com>
2019-06-09 20:46:03 +0530

    Refactor - move game setup to @BeforeEach to reduce duplicate code

--- src/test/java/com/personal/kata/TennisGameTest.java
+++ src/test/java/com/personal/kata/TennisGameTest.java
@@ -1,5 +1,6 @@
 package com.personal.kata;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,10 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TennisGameTest {
 
+    TennisGame tennisGame;
+
+    @BeforeEach
+    public void newGameSetup() {
+        tennisGame = new TennisGame();
+    }
+
     @Test
     @DisplayName("Given a tennis game When tennis game starts Then the two players scores are at zero each")
     public void test_NewGameState_ShouldHaveTwoPlayerScores_AtZeroEach() {
-        TennisGame tennisGame = new TennisGame();
 
         assertEquals(0, tennisGame.getPlayer1().getPlayerScore());
         assertEquals(0, tennisGame.getPlayer2().getPlayerScore());
@@ -19,7 +26,6 @@ public class TennisGameTest {
     @Test
     @DisplayName("Given a tennis game When tennis game starts Then there should be two players named Player 1 and Player 2")
     public void test_NewGameState_ShouldHaveTwoPlayerNames_Player1_Player2() {
-        TennisGame tennisGame = new TennisGame();
 
         assertEquals("Player 1", tennisGame.getPlayer1().getPlayerName());
         assertEquals("Player 2", tennisGame.getPlayer2().getPlayerName());
